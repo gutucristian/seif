f7147a632677cb1728401da229f518ecc26ad586
Cristian <gutucristian@hotmail.com>
2016-03-18 21:12:55 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// spawn pyrelic library
+// var spawn = require("child_process").spawn;
+// var process = spawn('python',["test.py", "add", 2, 3]);
+
+// process.stdout.on('data', function (data) {
+//     console.log('from python script: ' + data);    
+// });
+
+// call packages we need
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from POST request
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = 8080;
+
+var router = express.Router();
+
+router.post('/init', function(req, res){
+    res.json({ message: 'ok' });
+})
+
+
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('server running on port: ' + port);
---
+++ test.py
@@ -0,0 +1,24 @@
+import sys
+
+functionToCall = sys.argv[1]
+arg1 = sys.argv[2]
+arg2 = sys.argv[3]
+
+print(str(functionToCall))
+print(arg1)
+print(arg2)
+print(functionToCall == "add")
+
+def add(a, b):
+    print(int(a) + int(b))
+    sys.stdout.flush()
+
+def subtract(a, b):
+    print(int(a) - int(b))
+    sys.stdout.flush()
+
+if (functionToCall == "add"):
+    add(arg1, arg2)
+
+if functionToCall == "subtract":
+    subtract(arg1, arg2)
